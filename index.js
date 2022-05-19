dca2ee4ad6c12b9e4f22ec684c631b93e64f5c93
Carlos Mario Muriel Galvis <cmmurielo@gmail.com>
2022-05-18 22:27:06 -0500

    Implementando Auth con JWT

--- index.js
+++ index.js
@@ -9,28 +9,51 @@ import User from "./models/user.model.js";
 
 const app = express()
 const port = 3000
-const mongoAtlasUri = 'mongodb+srv://admin:<password>@cluster0.gbfnw.mongodb.net/todo?retryWrites=true&w=majority'
-
+const mongoAtlasUri = 'mongodb+srv://admin:<PASSWORD>@cluster0.gbfnw.mongodb.net/todo?retryWrites=true&w=majority'
 mongoose.connect(mongoAtlasUri)
 
 app.use(express.json())
 app.use(cors())
 
+const signToken = _id => jwt.sign({_id}, 'mi-secreto')
+
 //AUTH
-app.post('/api/auth', async (require, response) => {
-    const {body} = require
+app.post('/api/auth', async (req, res) => {
+    const {body} = req
     try {
         const isUser = await User.findOne({username: body.username})
         if (isUser) {
-            return response.status(403).send('El usuario ya existe.')
+            return res.status(403).send('El usuario ya existe.')
         }
         const salt = await bcrypt.genSalt()
         const hashed = await bcrypt.hash(body.password, salt)
         const user = await User.create({username: body.username, password: hashed, salt})
-        response.sendStatus(200)
+        const signed = signToken(user._id)
+        res.status(200).send(signed)
     } catch (e) {
         console.log(e)
         response.status(500).send(e.message)
+    }
+})
+
+//LOGIN
+app.post('/api/login', async (req, res) => {
+    const { body } = req
+    try {
+        const user = await User.findOne({username: body.username})
+        if (!user) {
+            res.send('Usuario o contrasena invalida')
+        } else {
+            const isMath = await bcrypt.compare(body.password, user.password)
+            if (isMath) {
+                const signed = signToken(user._id)
+                res.status(200).send(signed)
+            } else {
+                res.status(403).send('Usuario o contrasena invalida')
+            }
+        }
+    } catch (e) {
+        res.status(500).send(e.message)
     }
 })
 
@@ -42,5 +65,5 @@ app.put('/api/:id', Task.update)
 app.delete('/api/:id', Task.delete)
 
 app.listen(port, () => {
-    console.log('La app esta corriendo');
+    console.log('La app esta corriendo en el puerto ' + port );
 })
